8aaf5b5094bbcb88983fccb60074b15f80272424
Matheus Bonafine <mathbonafine@gmail.com>
2021-05-24 21:52:19 -0300

    fetch animais  refatorado

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
+import fetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import Tooltip from "./modules/tooltip.js";
 
@@ -38,5 +37,6 @@ initAnimacaoScroll();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initFetchBitcoin();
+
+fetchAnimais("././animaisapi.json", ".numeros-grid");
